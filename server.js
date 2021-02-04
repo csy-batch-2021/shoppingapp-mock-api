2c84f61efdcd50b598e9438aeee5ca194e7ba2dd
Naresh Kumar H <nareshkumarh@live.com>
2021-02-04 10:08:35 +0530

    Updated

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 //import {} from '@angular/core
 const jsonServer = require('json-server')
-
+const cors= require('cors');
 const server = jsonServer.create()
 
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
  
 server.use(middlewares)
+server.use(cors());
+
 server.use('/api', router)
 server.listen(process.env.PORT || 5000, () => {
   console.log('JSON Server is running')
